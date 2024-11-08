80a159f51658a2646dbb8e0b83d4d4786b734aa9
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-11-08 22:35:27 +0800

    added

--- app/components/Navbars.tsx
+++ app/components/Navbars.tsx
@@ -9,52 +9,31 @@ export default function Navbars() {
     <nav className="flex space-x-5">
       <Link
         href="/"
-        className={`${pathname === "/" ? "text-black border-b-2 border-transparent border-black" : "text-gray-500"
+        className={`${pathname === "/" ? "text-black" : "text-gray-500"
           } pb-1 border-b-2 border-transparent hover:border-black transition-all duration-300`}
       >
         Home
       </Link>
       <Link
-        href="/page/about"
-        className={`${pathname === "/page/about" ? "text-black border-b-2 border-transparent border-black" : "text-gray-500"
-          } pb-1 border-b-2 border-transparent hover:border-black transition-all duration-300`}
-      >
-        About Us
-      </Link>
-      <Link
         href="/page/product/what"
-        className={`${pathname === "/page/product/what" ? "text-black border-b-2 border-transparent border-black" : "text-gray-500"
+        className={`${pathname === "/page/product/what" ? "text-black" : "text-gray-500"
           } pb-1 border-b-2 border-transparent hover:border-black transition-all duration-300`}
       >
-        Trade
+        Contact
       </Link>
       <Link
         href="/page/product/puta"
-        className={`${pathname === "/page/product/puta" ? "text-black border-b-2 border-transparent border-black" : "text-gray-500"
+        className={`${pathname === "/page/product/puta" ? "text-black" : "text-gray-500"
           } pb-1 border-b-2 border-transparent hover:border-black transition-all duration-300`}
       >
-        Market
+        About
       </Link>
       <Link
         href="/page/about"
-        className={`${pathname === "/page/about" ? "text-black border-b-2 border-transparent border-black" : "text-gray-500"
+        className={`${pathname === "/page/about" ? "text-black" : "text-gray-500"
           } pb-1 border-b-2 border-transparent hover:border-black transition-all duration-300`}
       >
-        Wallet
-      </Link>
-      <Link
-        href="/page/about"
-        className={`${pathname === "/page/about" ? "text-black border-b-2 border-transparent border-black" : "text-gray-500"
-          } pb-1 border-b-2 border-transparent hover:border-black transition-all duration-300`}
-      >
-        Staking
-      </Link>
-      <Link
-        href="/page/about"
-        className={`${pathname === "/page/about" ? "text-black border-b-2 border-transparent border-black" : "text-gray-500"
-          } pb-1 border-b-2 border-transparent hover:border-black transition-all duration-300`}
-      >
-        Resources
+        Sign Up
       </Link>
     </nav>
   );
