50213abd0d14648525898c52c041aebfb814764e
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-11-08 20:37:37 +0800

    navbars

--- app/components/Navbars.tsx
+++ app/components/Navbars.tsx
@@ -7,8 +7,8 @@ export default function Navbars() {
   return (
       <nav>
           <Link className={pathname === '/' ? " font-bold mr-5" : " text-cyan-400 mr-5"} href={'/'}>Home</Link>
-          <Link className={pathname === '/page/product/2' ? " font-bold mr-5" : " text-cyan-400 mr-5"} href={'/page/product/2'}>Product</Link>
-          <Link className={pathname === '/page/product/puta' ? " font-bold mr-5" : " text-cyan-400 mr-5"} href={'/page/product/puta'}>Puta</Link>
+          <Link className={pathname === '/page/product/what' ? " font-bold mr-5" : " text-cyan-400 mr-5"} href={'/page/product/what'}>Product</Link>
+          <Link className={pathname === '/page/product/puta' ? " font-bold mr-5" : " text-cyan-400 mr-5 hover:font-bold hover:text-white transition-all"} href={'/page/product/puta'}>Puta</Link>
           <Link className={pathname === '/page/about' ? " font-bold mr-5" : " text-cyan-400 mr-5"} href={'/page/about'}>About</Link>
      </nav>
   )
