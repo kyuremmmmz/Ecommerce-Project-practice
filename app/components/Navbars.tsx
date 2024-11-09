3dce8c797dc9ffe8df16a2da323b7c73423144c6
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-11-09 23:32:19 +0800

    navs

--- app/components/Navbars.tsx
+++ app/components/Navbars.tsx
@@ -7,13 +7,13 @@ export default function Navbars() {
   const pathname = usePathname();
   return (
     <nav className="grid grid-cols-6 items-center gap-10">
-      <div className="col-span-2 mr-24">
+      <div className="col-span-1 ml-30">
         <h1 className="font-bold text-3xl text-right">Exclusive</h1>
       </div>
-      <div className="col-span-2 flex justify-center space-x-10">
+      <div className="col-span-4 flex justify-center space-x-8">
         <Link
           href="/"
-          className={`${pathname === "/" ? "text-black" : "text-gray-500"
+          className={`${pathname === "/" ? " border-b-2 border-black" : "text-gray-500"
             } relative text-black cursor-pointer transition-all duration-300 ease-in-out 
         before:absolute before:bottom-0 before:left-0 before:h-[2px]
         before:w-0 before:bg-black before:transition-all before:duration-300
@@ -23,7 +23,7 @@ export default function Navbars() {
         </Link>
         <Link
           href="/page/product/what"
-          className={`${pathname === "/page/product/what" ? "text-black" : "text-gray-500"
+          className={`${pathname === "/page/product/what" ? " border-b-2 border-black" : "text-gray-500"
             } relative text-black cursor-pointer transition-all duration-300 ease-in-out 
         before:absolute before:bottom-0 before:left-0 before:h-[2px]
         before:w-0 before:bg-black before:transition-all before:duration-300
@@ -33,7 +33,7 @@ export default function Navbars() {
         </Link>
         <Link
           href="/page/product/puta"
-          className={`${pathname === "/page/product/puta" ? "text-black" : "text-gray-500"
+          className={`${pathname === "/page/product/puta" ? " border-b-2 border-black" : "text-gray-500"
             } relative text-black cursor-pointer transition-all duration-300 ease-in-out 
         before:absolute before:bottom-0 before:left-0 before:h-[2px]
         before:w-0 before:bg-black before:transition-all before:duration-300
@@ -42,8 +42,18 @@ export default function Navbars() {
           About
         </Link>
         <Link
+          href="/page/product/puta"
+          className={`${pathname === "/page/product/puta" ? " border-b-2 border-black" : "text-gray-500"
+            } relative text-black cursor-pointer transition-all duration-300 ease-in-out 
+        before:absolute before:bottom-0 before:left-0 before:h-[2px]
+        before:w-0 before:bg-black before:transition-all before:duration-300
+        hover:before:w-full`}
+        >
+          Products
+        </Link>
+        <Link
           href="/page/about"
-          className={`${pathname === "/page/about" ? "text-black" : "text-gray-500"
+          className={`${pathname === "/page/about" ? " border-b-2 border-black" : "text-gray-500"
             } relative text-black cursor-pointer transition-all duration-300 ease-in-out
         before:absolute before:bottom-0 before:left-0 before:h-[2px]
         before:w-0 before:bg-black before:transition-all before:duration-300
