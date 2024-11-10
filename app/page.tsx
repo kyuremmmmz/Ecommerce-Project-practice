13ce553ff440c1baf6ed83406d8ca86633b3cd33
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-11-11 00:20:12 +0800

    gago

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,48 @@
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Home() {
+  const productsList = [
+    {
+      name: "Woman Fashion"
+    },
+    {
+      
+      name: "Men’s Fashion"
+    },
+    {
+      name: "Electronics"
+    },
+    {
+      name: "Home & Lifestyle"
+    },
+    {
+      name: "Medicine"
+    },
+    {
+      name: "Sports & Outdoor"
+    },
+    {
+      name: "Baby’s & Toys"
+    },
+    {
+      name: "Groceries & Pets"
+    },
+  ]
   return (
     <div className="grid grid-rows-[20px_1fr_20px] min-h-screen sticky p-8 pb-80 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="">
+      <main className="grid grid-cols-1 gap-2">
+        <div className="border-r-2 bg-black w-2">
+          {productsList.map((item, index) => {
+            return (
+              <div key={index}>
+                <div className=" container grid-rows-2 w-44 px-4 rounded hover:bg-gray-600 hover:text-white transition-all duration-300 cursor-pointer">
+                  <p>{item.name}</p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
       </main>
     </div>
   );
