917d76b76aa475f1e4947c8ef7d9ec7abc313683
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-11-12 20:47:15 +0800

    gago

--- app/components/ui/Promo/Promos.tsx
+++ app/components/ui/Promo/Promos.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Promos() {
   return (
-    <div>Promos</div>
+      <div className='flex flex-row justify-center items-center gap-2'>
+          <div className=' h-[490px] w-96 bg-black'>
+              fsdfs
+          </div>
+        </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,22 @@ import Promos from "./components/ui/Promo/Promos";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8  gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="grid grid-cols-2">
-        <Promos/>
+      <main className="flex justify-center">
+        <div className=" grid grid-cols-2 gap-1">
+          <Promos />
+          <div className=" grid grid-cols-1 gap-2">
+            <div className='flex flex-row justify-center items-center gap-2'>
+              <div className=' h-60 w-96 bg-black'>
+                fsdfs
+              </div>
+            </div>
+            <div className='flex flex-row justify-center items-center gap-2'>
+              <div className=' h-60 w-96 bg-black'>
+                fsdfs
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
