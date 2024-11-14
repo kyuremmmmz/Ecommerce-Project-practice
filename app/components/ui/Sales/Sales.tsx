cc867f0a9cb72a42b854ec68764fc55b2f359df9
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-11-14 18:03:45 +0800

    added tanginang yan gago

--- app/components/ui/Sales/Sales.tsx
+++ app/components/ui/Sales/Sales.tsx
@@ -9,9 +9,22 @@ function Sales() {
                   <h2 className=' font-semibold text-red-500'>Today&apos;s</h2>
                   <div className=' w-[1000px]'></div>
               </div>
-              <div className=' flex flex-row gap-2'>
-                  <h2 className=' font-semibold text-4xl'>Flash Sales</h2>
+              <div className="flex flex-row gap-2 items-center">
+                  <h2 className="font-semibold text-4xl">Flash Sales</h2>
+                  <div className="flex flex-col items-center mb-4">
+                      <p className="text-sm font-semibold">Days</p>
+                      <h2 className="text-3xl font-semibold leading-none">03</h2>
+                  </div>
+                  <div className=' flex flex-col items-center justify-center text-center mb-1'>
+                      <div className=' h-1'></div>
+                      <h2 className='text-3xl font-semibold leading-none text-red-400'>:</h2>
+                  </div>
+                  <div className="flex flex-col items-center justify-center text-center mb-4">
+                      <p className="text-sm font-semibold">Hours</p>
+                      <h2 className="text-3xl font-semibold leading-none">23</h2>
+                  </div>
               </div>
+
               
         </div>
     </div>
