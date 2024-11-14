cacff7dc62e14768372fafe7fab339cbda664ca2
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-11-14 16:25:01 +0800

    added puta

---
+++ app/components/ui/Sales/Sales.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Sales() {
+  return (
+    <div className=' flex'>
+        <div className=' flex flex-row gap-2 items-center justify-center'>
+            <div className=' w-4 h-12 bg-red-500 rounded'></div>
+            <h2 className=' font-semibold text-red-500'>Today&apos;s</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Sales
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import  Promos  from "./components/ui/Promo/Promos";
 import Promos2 from "./components/ui/Promo/Promos2";
+import Sales from "./components/ui/Sales/Sales";
 
 export default function Home() {
   return (
-    <div className="grid h-screen p-8  gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid h-screen-full p-8  gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <section>
           <div className="md:flex-row flex flex-col justify-center items-stretch gap-2">
               <Promos />
@@ -11,8 +12,13 @@ export default function Home() {
               <Promos2 />
               <Promos2 />
             </div>
-          </div>
-        </section>
+        </div>
+      </section>
+      <section className=" ">
+        <div className=" flex justify-center">
+          <Sales />
+        </div>
+      </section>
     </div>
   );
 }
