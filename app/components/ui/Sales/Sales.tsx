5618a13d52ec8bb3afc3c924be5f45b1c3e76158
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-11-15 00:16:22 +0800

    product item

--- app/components/ui/Sales/Sales.tsx
+++ app/components/ui/Sales/Sales.tsx
@@ -1,3 +1,4 @@
+import { Badge } from '@/components/ui/badge'
 import React from 'react'
 
 function Sales() {
@@ -9,23 +10,53 @@ function Sales() {
                   <h2 className=' font-semibold text-red-500'>Today&apos;s</h2>
                   <div className=' w-[1000px]'></div>
               </div>
-              <div className="flex flex-row gap-2 items-center">
+              <div className="flex flex-row gap-10 items-center">
                   <h2 className="font-semibold text-4xl">Flash Sales</h2>
-                  <div className="flex flex-col items-center mb-4">
-                      <p className="text-sm font-semibold">Days</p>
-                      <h2 className="text-3xl font-semibold leading-none">03</h2>
-                  </div>
-                  <div className=' flex flex-col items-center justify-center text-center mb-1'>
-                      <div className=' h-1'></div>
-                      <h2 className='text-3xl font-semibold leading-none text-red-400'>:</h2>
-                  </div>
-                  <div className="flex flex-col items-center justify-center text-center mb-4">
-                      <p className="text-sm font-semibold">Hours</p>
-                      <h2 className="text-3xl font-semibold leading-none">23</h2>
+                  <div className=' flex flex-row gap-8'>
+                      <div className="flex flex-col items-center mb-4">
+                          <p className="text-sm font-semibold">Days</p>
+                          <h2 className="text-3xl font-semibold leading-none">03</h2>
+                      </div>
+                      <div className=' flex flex-col items-center justify-center text-center mb-1'>
+                          <div className=' h-1'></div>
+                          <h2 className='text-3xl font-semibold leading-none text-red-400'>:</h2>
+                      </div>
+                      <div className="flex flex-col items-center justify-center text-center mb-4">
+                          <p className="text-sm font-semibold">Hours</p>
+                          <h2 className="text-3xl font-semibold leading-none">23</h2>
+                      </div>
+                      <div className=' flex flex-col items-center justify-center text-center mb-1'>
+                          <div className=' h-1'></div>
+                          <h2 className='text-3xl font-semibold leading-none text-red-400'>:</h2>
+                      </div>
+                      <div className="flex flex-col items-center justify-center text-center mb-4">
+                          <p className="text-sm font-semibold">Minutes</p>
+                          <h2 className="text-3xl font-semibold leading-none">19</h2>
+                      </div>
+                      <div className=' flex flex-col items-center justify-center text-center mb-1'>
+                          <div className=' h-1'></div>
+                          <h2 className='text-3xl font-semibold leading-none text-red-400'>:</h2>
+                      </div>
+                      <div className="flex flex-col items-center justify-center text-center mb-4">
+                          <p className="text-sm font-semibold">Seconds</p>
+                          <h2 className="text-3xl font-semibold leading-none">56</h2>
+                      </div>
                   </div>
               </div>
-
-              
+              <div className=' flex flex-row gap-6'>
+                  <div className=' w-56 h-40 bg-slate-200'>
+                      <Badge variant="destructive" className=' bg-red-500 rounded absolute mt-4 mx-3 hover:bg-red-200 duration-300 cursor-pointer'>40%</Badge>
+                  </div>
+                  <div className=' w-56 h-40 bg-slate-200'>
+                      <Badge variant="destructive" className=' bg-red-500 rounded absolute mt-4 mx-3 hover:bg-red-200 duration-300 cursor-pointer'>40%</Badge>
+                  </div>
+                  <div className=' w-56 h-40 bg-slate-200'>
+                      <Badge variant="destructive" className=' bg-red-500 rounded absolute mt-4 mx-3 hover:bg-red-200 duration-300 cursor-pointer'>40%</Badge>
+                  </div>
+                  <div className=' w-56 h-40 bg-slate-200'>
+                      <Badge variant="destructive" className=' bg-red-500 rounded absolute mt-4 mx-3 hover:bg-red-200 duration-300 cursor-pointer'>40%</Badge>
+                  </div>
+              </div>
         </div>
     </div>
   )
