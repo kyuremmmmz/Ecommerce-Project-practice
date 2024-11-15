164806047c00a657c94ec7363f558bf215ed72ed
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-11-15 19:32:57 +0800

    gago

--- app/components/ui/Sales/Sales.tsx
+++ app/components/ui/Sales/Sales.tsx
@@ -1,6 +1,8 @@
 import { Badge } from '@/components/ui/badge'
 import React from 'react'
-
+import { FaStar } from 'react-icons/fa';
+import image  from '../../../../assets/product-removebg-preview.png';
+import Image from 'next/image';
 function Sales() {
     const list: unknown[] = [
         {
@@ -67,17 +69,30 @@ function Sales() {
                   {list.map((item, index) => {
                       return (
                           <div key={index} className=' w-auto bg-white h-72'>
-                              <div className="relative w-56 h-40 bg-slate-100 overflow-hidden group/item">
+                              <div className="relative w-64 h-64 bg-slate-100 overflow-hidden group/item">
                                   <Badge variant="destructive" className="bg-red-500 rounded absolute mt-4 mx-3 hover:bg-red-200 duration-300 cursor-pointer text-white">
                                       -40%
                                   </Badge>
-                                  <div className=' flex justify-center bg-black  my-72 items-center text-center hover:bg-slate-700 group-hover/item:my-[135px] duration-300'>
+                                  <div className=' flex mx-5 my-5'>
+                                      <Image className=' absolute justify-center items-center size-56' src={image} alt={'img'} />
+                                  </div>
+                                  <div className=' flex z-50 justify-center bg-black  my-72 items-center text-center hover:bg-slate-700 group-hover/item:my-[210px] duration-300'>
                                       <h1 className=' text-white font-normal cursor-pointer'>{item.name}</h1>
                                   </div>
                               </div>
                               <div>
                                   <h1 className=' font-medium'>HAVIT HV-G92 Gamepad</h1>
                                   <p className=' text-red-400 font-medium'>$120</p>
+                                  <div className=' flex flex-row gap-3'>
+                                      <div className='flex flex-row'>
+                                          <FaStar color='orange' />
+                                          <FaStar color='orange' />
+                                          <FaStar color='orange' />
+                                          <FaStar color='orange' />
+                                          <FaStar color='gray' />
+                                      </div>
+                                      <p className=' text-slate-400 '>(75)</p>
+                                  </div>
                               </div>
                           </div>
                       )
