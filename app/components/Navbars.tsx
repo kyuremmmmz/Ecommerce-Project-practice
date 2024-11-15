e6340149125456dfdad0272015688db48d4ebfee
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-11-15 13:12:11 +0800

    added some shits

--- app/components/Navbars.tsx
+++ app/components/Navbars.tsx
@@ -88,7 +88,6 @@ export default function Navbars() {
                     onClick={() => setSign(true)}>Sign Up</button>
                 </div>
                 )
-
               )
             }
           </div>
