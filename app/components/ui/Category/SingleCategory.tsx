615349fcd9bca7b26d2e094b65fdb6c3db1eaaf5
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-11-18 16:03:57 +0800

    yey animation nalangzzz

--- app/components/ui/Category/SingleCategory.tsx
+++ app/components/ui/Category/SingleCategory.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import BuyNowButton from '../Buttons/BuyNowButton'
-
+import Image from 'next/image'
+import radio from '../../../../assets/jbl-boombox-removebg-preview.png';
 function SingleCategory() {
   return (
-    <div className=' bg-black flex flex-row w-[1170px] h-[500px] gap-16'>
+    <div className=' bg-black flex flex-row w-[1200px] h-[550px] gap-10 overflow-hidden'>
           <div className=' flex flex-col gap-2'>
               <p className=' text-green-400 px-10 mt-10 py-5 font-semibold'>Categories</p>
               <div className=' flex flex-col gap-2'>
@@ -32,7 +33,13 @@ function SingleCategory() {
                 <BuyNowButton/>
               </div>
           </div>
-          <div className=' flex justify-center bg-slate-50 opacity-30 blur-xl w-96 h-96 rounded-full my-16'></div>
+          <div className=' flex justify-center w-[550px] h-[500px]  rounded-full my-5'>
+              <div className='flex justify-center bg-slate-50 opacity-30 blur-xl w-[550px] h-[500px] rounded-full my-5'>
+              </div>
+              <div className=' absolute flex items-center justify-center my-20'>
+                  <Image src={radio} alt={'radio'} className='scale-90' />
+              </div>
+          </div>
     </div>
   )
 }
