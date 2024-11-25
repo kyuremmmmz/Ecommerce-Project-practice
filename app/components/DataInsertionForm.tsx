e227a0c844ff3f975f1da4af386dbf22ab40b906
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-11-25 23:00:37 +0800

    this tasks will be continue tomo

--- app/components/DataInsertionForm.tsx
+++ app/components/DataInsertionForm.tsx
@@ -4,17 +4,35 @@ import TextAreaWide from './ui/Fields/TextAreaWide';
 
 function DataInsertionForm() {
   return (
-      <div className=' flex flex-row justify-center items-center h-screen-full'>
-          <div className=' flex flex-col py-10 '>
-              <h1 className=' font-medium text-2xl'>Post my Product</h1>
+      <div className=' flex flex-row h-screen-full'>
+          <div className=' flex flex-col px-72 '>
+              <h1 className=' font-medium text-2xl py-6'>Post my Product</h1>
               <form className=' flex flex-col gap-2'>
                   <label>Product Name: </label>
                   <TextAreaFullBorder type='' value='' onChange={(e) => e.target.value} />
                   <label>Product Description: </label>
                   <TextAreaWide value='' onChange={(e) => e.target.value} placeHolder={''} />
-                  <TextAreaFullBorder type='text' value='' onChange={(e) => e.target.value} />
-                  <TextAreaFullBorder type='text' value='' onChange={(e) => e.target.value} />
-                  <TextAreaFullBorder type='text' value='' onChange={(e) => e.target.value} />
+                  <label>Product Price: </label>
+                  <TextAreaFullBorder type='number' value='' onChange={(e) => e.target.value} />
+                  <label>Product Discount: </label>
+                  <TextAreaFullBorder type='number' value='' onChange={(e) => e.target.value} />
+                  <label>Product Image: </label>
+                  <TextAreaFullBorder type='file' value='' onChange={(e) => e.target.value} />
+              </form>
+          </div>
+          <div className=' flex flex-col px-72 '>
+              <h1 className=' font-medium text-2xl py-6'>Post my Product</h1>
+              <form className=' flex flex-col gap-2'>
+                  <label>Product Name: </label>
+                  <TextAreaFullBorder type='' value='' onChange={(e) => e.target.value} />
+                  <label>Product Description: </label>
+                  <TextAreaWide value='' onChange={(e) => e.target.value} placeHolder={''} />
+                  <label>Product Price: </label>
+                  <TextAreaFullBorder type='number' value='' onChange={(e) => e.target.value} />
+                  <label>Product Discount: </label>
+                  <TextAreaFullBorder type='number' value='' onChange={(e) => e.target.value} />
+                  <label>Product Image: </label>
+                  <TextAreaFullBorder type='file' value='' onChange={(e) => e.target.value} />
               </form>
           </div>
     </div>
--- app/page/product/page.tsx
+++ app/page/product/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='grid p-8 h-screen-full  gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+    <div className='grid p-5 h-screen-full  gap-16 sm:p-8 font-[family-name:var(--font-geist-sans)]'>
       <DataInsertionForm/>
     </div>
   )
